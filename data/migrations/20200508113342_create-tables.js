1ead32f337a742abd30c489f76f9e43f828a164f
Fessqer <42286154+Fessqer@users.noreply.github.com>
2020-12-23 00:44:17 +0300

    Update 20200508113342_create-tables.js

--- data/migrations/20200508113342_create-tables.js
+++ data/migrations/20200508113342_create-tables.js
@@ -2,7 +2,6 @@ exports.up = function (knex) {
   return knex.schema
     .createTable("post", (table) => {
       table.increments();
-      table.string("link").notNullable();
       table.string("title").notNullable().index();
       table.text("content").notNullable();
       table.datetime("created_at").defaultTo(knex.fn.now());
